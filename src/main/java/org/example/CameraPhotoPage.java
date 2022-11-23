f7b6345af6fe0f34c32a11082dedcc33bc4a3e7d
letslearntestingnow <letslearntestingnow@gmail.com>
2022-11-23 21:40:04 +0000

    Added methods.

--- src/main/java/org/example/CameraPhotoPage.java
+++ src/main/java/org/example/CameraPhotoPage.java
@@ -7,13 +7,31 @@ import java.util.List;
 
 public class CameraPhotoPage extends Utils
 {
+    private By _ListOfProductNames_ClassName = By.className("product-title");
+
+    private By _ListOfProductsComplete_ClassName = By.className("details");
+
     public void printProductNames()
     {
-        List<WebElement> listCameras = driver.findElements(By.xpath("//h2 [@class = 'product-title']"));
+        List<WebElement> listCameras = getAllElementsMatchingThisLocator(_ListOfProductNames_ClassName);
         for(WebElement ele: listCameras)
         {
             System.out.println(ele.getText());
         }
+    }
 
+    public void checkProductsForAddToCartButton()
+    {
+        List<WebElement> listCameras = getAllElementsMatchingThisLocator(_ListOfProductsComplete_ClassName);
+
+        for(WebElement ele: listCameras)
+        {
+        if (!(ele.getText().contains("ADD TO CART")))
+            {
+                //System.out.println(ele.getText());
+                System.out.println(ele.findElement(By.className("product-title")).getText());
+            }
+
+        }
     }
 }
